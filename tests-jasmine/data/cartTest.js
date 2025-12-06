9ba46bb2f9f74932dccf441ff0f1b3a577deb24e
Sakibul Hasan Rafi <iamshrafi@gmail.com>
2025-12-06 15:27:38 +0600

    automated tests addToCart

---
+++ tests-jasmine/data/cartTest.js
@@ -0,0 +1,43 @@
+import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
+
+describe('test suite: add to cart', () => {
+    it('adds an exixting product to the cart', () => {
+        spyOn(localStorage, 'setItem');
+
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            // localStorage only support strings
+            return JSON.stringify([{
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1,
+                deliveryOptionId: '1'
+            }]);
+        });
+
+        loadFromStorage();
+
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.length).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart[0].quantity).toEqual(2);
+    });
+
+    it('adds a new product to the cart', () => {
+        spyOn(localStorage, 'setItem');
+
+        // from jasmine use mock to create a fake version of get item
+        // take two parameters. first object want to mock, second method want to mock
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            // localStorage only support strings
+            return JSON.stringify([]);
+        });
+
+        loadFromStorage();
+
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.length).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart[0].quantity).toEqual(1);
+    });
+});
