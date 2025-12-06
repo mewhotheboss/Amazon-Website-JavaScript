092ffa5c4fb73949334e90da723304eb5b97775e
Sakibul Hasan Rafi <iamshrafi@gmail.com>
2025-12-06 18:24:54 +0600

    automated orderSummary

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -1,23 +1,29 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
-import { loadFromStorage } from "../../data/cart.js";
+import { loadFromStorage, cart } from "../../data/cart.js";
 
 describe('test suite: renderOrderSummary', () => {
-    it('displayes the cart', () => {
+    const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+    const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+    beforeEach(() => {
+        spyOn(localStorage, 'setItem');
+
         document.querySelector('.js-test-container').innerHTML = `
         <div class="js-order-summary"></div>
+        <div class="js-payment-summary"></div>
         `;
 
         spyOn(localStorage, 'getItem').and.callFake(() => {
             // localStorage only support strings
             return JSON.stringify([
                 {
-                    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                    productId: productId1,
                     quantity: 2,
                     deliveryOptionId: '1'
                 },
 
                 {
-                    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                    productId: productId2,
                     quantity: 1,
                     deliveryOptionId: '2'
                 }
@@ -26,9 +32,42 @@ describe('test suite: renderOrderSummary', () => {
 
         loadFromStorage();
         renderOrderSummary();
+    });
 
+    it('displayes the cart', () => {
         expect(
             document.querySelectorAll('.js-cart-item-container').length
         ).toEqual(2);
+
+        expect(
+            document.querySelector(`.js-product-quantity-${productId1}`).innerText
+        ).toContain('Quantity: 2');
+
+        expect(
+            document.querySelector(`.js-product-quantity-${productId2}`).innerText
+        ).toContain('Quantity: 1');
+
+        document.querySelector('.js-test-container').innerHTML = '';
+    });
+
+    it('removes a product', () => {
+
+        document.querySelector(`.js-delete-link-${productId1}`).click();
+        expect(
+            document.querySelectorAll('.js-cart-item-container').length
+        ).toEqual(1);
+
+        expect(
+            document.querySelector(`.js-cart-item-container-${productId1}`)
+        ).toEqual(null);
+
+        expect(
+            document.querySelector(`.js-cart-item-container-${productId2}`)
+        ).not.toEqual(null);
+
+        expect(cart.length).toEqual(1);
+        expect(cart[0].productId).toEqual(productId2);
+
+        document.querySelector('.js-test-container').innerHTML = '';
     });
 });
