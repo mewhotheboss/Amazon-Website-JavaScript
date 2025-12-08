d07f6a1961b24a2432a4d414096e45d3ac715311
Sakibul Hasan Rafi <iamshrafi@gmail.com>
2025-12-08 17:27:34 +0600

    convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,52 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js'
 
-loadProducts(() => {
+// built in class need to give a function
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1');
+        });
+    }),
+
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+]).then((values) => {
+    console.log(values);
+    renderOrderSummary();
+    renderPaymentSummary();
+})
+
+/*
+// built in class need to give a function
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+}).then(() => {
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+}).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
+
+/*
+loadProducts(() => {
+    loadCart(() => {
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
+});
+*/
