55a738060eccaf2904afa4b427ed0edb51998ce9
Sakibul Hasan Rafi <iamshrafi@gmail.com>
2025-12-17 12:42:17 +0600

    orderSummary updated

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -34,23 +34,6 @@ function renderProductsGrid(productsToRender) {
           ${product.getPrice()}
         </div>
 
-        <div class="product-quantity-container">
-          <select>
-            <option selected value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
-        </div>
-
-        ${product.extraInfoHTML()}
-
         <div class="product-spacer"></div>
 
         <div class="added-to-cart">
